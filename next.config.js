8336cbf054749eea918e653a63fcbd585cfd93a8
Ryan Zhang <ryan@itsryan.org>
2023-02-26 23:12:31 -0700

    prelim style changes, centralized color palette

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "styles/variables.globals.scss";`
+  }
 }
 
 module.exports = nextConfig
