29fba770da423fb66e8a947fea47ab9aab1f4bc8
shanecranor <shanecranor@gmail.com>
2023-02-28 18:13:43 -0700

    please build please

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
-
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@import "variables.globals.scss";`
   }
+  
 }
 
 module.exports = nextConfig
