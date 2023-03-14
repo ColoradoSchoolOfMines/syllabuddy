9a64841abe2afd0f0fa329b1f4eb6aa06f1784fa
Ryann Zhang <ryan@itsryan.org>
2023-03-14 16:47:37 -0600

    final landing page changes for 3/14, filler "what is" text

--- components/LandingAbout.tsx
+++ components/LandingAbout.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function LandingAbout({shown}: {shown: boolean}) {
   return (
     <div className={`${styles.landingAbout} ${shown?'':styles.hideLandingDivs}`}>
-      <h2>What is Syllabuddy?</h2>
+      <h2 onClick={()=>{alert("apologies, our about text has not been written yet. it's in progress. thanks!")}}>What is Syllabuddy?</h2>
       <div className={styles.filler}></div>
       <p>Search above or scroll down to explore courses</p>
     </div>
