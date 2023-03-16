0a091fedc953188a81983b17325b7170efd2d889
shanecranor <shanecranor@gmail.com>
2023-03-16 16:45:48 -0600

    added the lil guy

--- components/LandingAbout.tsx
+++ components/LandingAbout.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function LandingAbout({shown}: {shown: boolean}) {
   return (
     <div className={`${styles.landingAbout} ${shown||styles.hideLandingDivs}`}>
-      <h2 onClick={()=>{alert("apologies, our about text has not been written yet. it's in progress. thanks!")}}>What is Syllabuddy? &nbsp; <a href="https://docs.google.com/forms/d/e/1FAIpQLScx6kpBzIJWUOSbspixLrMWfeVh6C6xHJFfZ_NvEsxSTo4QKg/viewform">Contribute Syllabi</a></h2>
+      <h2><a onClick={()=>{alert("apologies, our about text has not been written yet. it's in progress. thanks!")}}>What is Syllabuddies?</a> &nbsp; <a href="https://docs.google.com/forms/d/e/1FAIpQLScx6kpBzIJWUOSbspixLrMWfeVh6C6xHJFfZ_NvEsxSTo4QKg/viewform">Contribute Syllabi</a></h2>
       <div className={styles.filler}></div>
       <p>Search above or scroll down to explore courses</p>
     </div>
