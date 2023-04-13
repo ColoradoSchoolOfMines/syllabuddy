a8e3a8ff50b61e10c2a26e66de070299302da8c6
shanecranor <shanecranor@gmail.com>
2023-04-12 19:47:46 -0600

    hover for about page

--- components/LandingAbout.tsx
+++ components/LandingAbout.tsx
@@ -7,7 +7,16 @@ export default function LandingAbout({shown}: {shown: boolean}) {
 
   return (
     <div className={`${styles.landingAbout} ${shown||styles.hideLandingDivs}`}>
-      <h2><a onClick={()=>{setShowDescription((oldDescription) => !oldDescription)}}>What is Syllabuddies?</a> <a href="https://docs.google.com/forms/d/e/1FAIpQLScx6kpBzIJWUOSbspixLrMWfeVh6C6xHJFfZ_NvEsxSTo4QKg/viewform">Contribute Syllabi</a></h2>
+      <h2>
+        <a onMouseEnter={()=>{setShowDescription(true)}}
+           onMouseLeave={()=>{setShowDescription(false)}}
+          >
+          What is Syllabuddies?
+        </a>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLScx6kpBzIJWUOSbspixLrMWfeVh6C6xHJFfZ_NvEsxSTo4QKg/viewform">
+            Contribute Syllabi
+        </a>
+      </h2>
       <div className={`${styles.landingAboutDesc} ${showDescription?'':styles.hideDiv}`}>
         <p>Syllabuddies is a way to quickly find syllabi for Colorado School of Mines courses so that you can make more informed decisions about which courses to register for.</p>
       </div>
